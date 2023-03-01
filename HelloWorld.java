85ac4c9d8d96a4d2e58e63d828ed28b6f127a776
lrhemmerlin <HemmerlinLR20@gcc.edu>
2023-03-01 13:48:06 -0500

    Got user name and told them hello

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String args[]){
-        System.out.println("HelloWorld!");
+        Scanner console = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = console.next();
+        System.out.println("Hello " + name);
         System.out.println("Commit test two");
     }
 }
